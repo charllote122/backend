a8d61ec162b4cd90bad79dd924acc203572ac758
charllote122 <sofichemjor@gmail.com>
2025-05-13 13:49:04 +0300

    Update index.js, package.json, and add package-lock.json

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('"Hello World!"');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
