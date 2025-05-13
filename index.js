8ba5abb675de09ed72bf1021e63a3ff25aadc17e
charllote122 <sofichemjor@gmail.com>
2025-05-13 12:15:56 +0300

    Fix: bind to Render port

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Node.js!");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
